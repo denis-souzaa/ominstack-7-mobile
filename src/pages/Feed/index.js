d4d015d257bd37aa69061917c74e09a7f2b2476b
Denis Souza <denyssouza1@hotmail.com>
2019-06-13 14:06:45 -0300

    initial commit

---
+++ src/pages/Feed/index.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import
+{
+  View,
+  Text,
+  Image,
+  TouchableOpacity,
+}
+  from 'react-native';
+
+// import { Container } from './styles';
+
+import camera from '~/assets/camera.png';
+
+export default class Feed extends Component {
+  static navigationOptions = ({ navigation }) => ({
+    headerRight: (
+      <TouchableOpacity onPress={() => navigation.navigate('New')} style={{ marginRight: 20 }}>
+        <Image source={camera} />
+      </TouchableOpacity>
+    ),
+  });
+
+  render() {
+    return (
+      <View>
+        <Text>Welcome to RocketSeat</Text>
+      </View>
+    );
+  }
+}
---
+++ src/pages/New/index.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+// import { Container } from './styles';
+
+export default class New extends Component {
+  render() {
+    return <View>
+        <Text>Welcome to RocketSeat</Text>
+    </View>;
+  }
+}
